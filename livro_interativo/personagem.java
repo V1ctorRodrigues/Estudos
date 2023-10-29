4de5e37f861121f4985b4378490196315ab309a5
Victor Rodrigues <victor.rodrigueslima@ufrpe.br>
2023-10-29 13:22:42 -0300

    Criação da nova classe

---
+++ livro_interativo/personagem.java
@@ -0,0 +1,12 @@
+package livro_interativo;
+
+public class personagem {
+    //Atributos da classe
+    String name;
+    int pontosDeVida;
+
+}
+public personagem(String name, int pontosDeVida){
+    this.name = name;
+    this.pontosDeVida = pontosDeVida;
+}
