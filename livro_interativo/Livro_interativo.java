0aef93e0118d3ecc3d8dac011b0697d562a29d1c
Victor Rodrigues <victor.rodrigueslima@ufrpe.br>
2023-10-30 14:56:39 -0300

    Sistema de resposta inválida criado

--- livro_interativo/Livro_interativo.java
+++ livro_interativo/Livro_interativo.java
@@ -1,37 +1,38 @@
 package livro_interativo;
+
 import java.util.Scanner;
 
 class Livro_interativo {
-    public static void main(String[] args) throws Exception {
-        //Escolhas para iniciar
-        String escolha1 = "SIM";
-        String escolha2 = "NAO";
-        String resposta;
+        public static void main(String[] args) throws Exception {
+                // Escolhas para iniciar
+                String escolha1 = "SIM";
+                String escolha2 = "NAO";
+                String resposta;
 
-        //Criação de scanner
-        Scanner leitor = new Scanner(System.in);
+                // Criação de scanner
+                Scanner leitor = new Scanner(System.in);
 
-        System.out.println("Olá! Você está em um livro interativo criado pelo\n" +
-                "aluno Victor Rodrigues, eu gostaria de informar\n" +
-                "que existem 2 tipos de interação nesse livro: \n" +
-                "'*' significa AÇÃO PARA O PERSONAGEM. \n" +
-                "'-' significa FALA PARA O PERSONAGEM.\n" +
-                "\nVamos começar? -"+ escolha1 + " -" + escolha2 + "\n");
+                System.out.println("Olá! Você está em um livro interativo criado pelo\n" +
+                                "aluno Victor Rodrigues, eu gostaria de informar\n" +
+                                "que existem 2 tipos de interação nesse livro: \n" +
+                                "'*' significa AÇÃO PARA O PERSONAGEM. \n" +
+                                "'-' significa FALA PARA O PERSONAGEM.\n" +
+                                "\nVamos começar? -" + escolha1 + " -" + escolha2 + "\n");
 
-        //Criação de um scanner com o nome Start
-        String init = leitor.nextLine();
+                // Criação de um scanner com o nome Start
+                String init = leitor.nextLine();
 
-        //Começar ou não o programa
-        if (init.equalsIgnoreCase(escolha1)) {
-        }
+                // Começar ou não o programa
+                if (init.equalsIgnoreCase(escolha1)) {
+                }
 
-        //Parar o funcionamento do programa 
-        else if(init.equalsIgnoreCase(escolha2)) {
-            System.out.println("\nTudo bem, fica para uma próxima :(\n");
-            System.exit(0);
-        }
+                // Parar o funcionamento do programa
+                else if (init.equalsIgnoreCase(escolha2)) {
+                        System.out.println("\nTudo bem, fica para uma próxima :(\n");
+                        System.exit(0);
+                }
 
-        //Corpo com a história
+                // Corpo com a história
                 String cap1 = "Começo do capítulo 1";
                 String choisecap1 = "Final 1"; // Para o final 1
                 String choise2cap1 = "Cap3"; // Para o cap3
@@ -46,26 +47,27 @@ class Livro_interativo {
 
                 String cap5 = "Final 3";
 
-        // Começo do Cap 1        
-         System.out.println("\n" + cap1 + "\n");
-         System.out.println(choisecap1 + " | " + choise2cap1 + "\n");
-        resposta = leitor.nextLine();
+                // Começo do Cap 1
+                System.out.println("\n" + cap1 + "\n");
+                System.out.println(choisecap1 + " | " + choise2cap1 + "\n");
+                resposta = leitor.nextLine();
 
-        //Final 1
-        if(resposta.equalsIgnoreCase(choisecap1)){
-          System.out.println(cap2);
+                // Final 1
+                if (resposta.equalsIgnoreCase(choisecap1)) {
+                        System.out.println(cap2);
 
-        //Capítulo 3
-        } else if(resposta.equalsIgnoreCase(choise2cap1)){
-                System.out.println("\n" + cap3 + "\n");
-                System.out.println(choisecap3 + " | " + choise2cap3 + "\n");
-                resposta = leitor.nextLine();  
-        //Final 2     
-        } if(resposta.equalsIgnoreCase(choisecap3)){
-                System.out.println(cap4);
-        //Final 3
-        } else if(resposta.equalsIgnoreCase(choise2cap3)){
-                System.out.println(cap5);
+                        // Capítulo 3
+                } else if (resposta.equalsIgnoreCase(choise2cap1)) {
+                        System.out.println("\n" + cap3 + "\n");
+                        System.out.println(choisecap3 + " | " + choise2cap3 + "\n");
+                        resposta = leitor.nextLine();
+                        // Final 2
+                }
+                if (resposta.equalsIgnoreCase(choisecap3)) {
+                        System.out.println(cap4);
+                        // Final 3
+                } else if (resposta.equalsIgnoreCase(choise2cap3)) {
+                        System.out.println(cap5);
+                }
         }
-    }
 }
