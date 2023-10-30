2196cb1f3d93aed419de9dc3cfb17db4c1d4f8a9
Victor Rodrigues <victor.rodrigueslima@ufrpe.br>
2023-10-30 12:04:09 -0300

    Organizando com ferramentas melhores

--- livro_interativo/Livro_interativo.java
+++ livro_interativo/Livro_interativo.java
@@ -9,49 +9,59 @@ class Livro_interativo {
                 "'*' significa AÇÃO PARA O PERSONAGEM \n" +
                 "'-' significa FALA PARA O PERSONAGEM.\n" +
                 "\nVamos começar?  -  Sim  - Nao\n");
+        String resposta;
+        //Escolhas para iniciar
+        String escolha1 = "sim";
+        String escolha2 = "nao";
+
         //Criação de um scanner com o nome Start
         Scanner leitor = new Scanner(System.in);
         String init = leitor.nextLine();
+
         //Começar ou não o programa
-        if (init.equalsIgnoreCase("sim")) {
+        if (init.equalsIgnoreCase(escolha1)) {
         }
+
         //Parar o funcionamento do programa 
-        else {
+        else if(init.equalsIgnoreCase(escolha2)) {
             System.out.println("\nTudo bem, fica para uma próxima :(\n");
             System.exit(0);
         }
-        //Capta todas as respostas
-                String resposta;
 
         //Corpo com a história
                 String cap1 = "Começo do capítulo 1";
-                String choisecap1 = "Vai para o final 1"; // Para o final 1
-                String choise2cap1 = "Vai para o cap3"; // Para o cap3
+                String choisecap1 = "Final 1"; // Para o final 1
+                String choise2cap1 = "Cap3"; // Para o cap3
 
                 String cap2 = "Começo do capítulo 2 - Final 1";
 
                 String cap3 = "Começo do capítulo 3";
-                String choisecap3 = "Vai para o final 2"; // Para o final 2
-                String choise2cap3 = "Vai para o final 3"; // Para o final 3
+                String choisecap3 = "Final 2"; // Para o final 2
+                String choise2cap3 = "Final 3"; // Para o final 3
 
                 String cap4 = "Começo do capítulo 4 - Final 2";
 
                 String cap5 = "Começo do capítulo 4 - Final 3";
 
         // Começo do Cap 1        
-        System.out.println("\n" + cap1 + "\n");
-        System.out.println(choisecap1 + " | " + choise2cap1 + "\n");
-        // Registrando escolha
+         System.out.println("\n" + cap1 + "\n");
+         System.out.println(choisecap1 + " | " + choise2cap1 + "\n");
         resposta = leitor.nextLine();
-        if(resposta == choisecap1){
+
+        //Final 1
+        if(resposta.equalsIgnoreCase(choisecap1)){
           System.out.println(cap2);
-        } else if(resposta == choise2cap1){
+
+        //Capítulo 3
+        } else if(resposta.equalsIgnoreCase(choise2cap1)){
                 System.out.println("\n" + cap3 + "\n");
                 System.out.println(choisecap3 + " | " + choise2cap3 + "\n");
-                resposta = leitor.nextLine();       
-        } if(resposta == choisecap3){
+                resposta = leitor.nextLine();  
+        //Final 2     
+        } if(resposta.equalsIgnoreCase(choisecap3)){
                 System.out.println(cap4);
-        } else if(resposta == choise2cap3){
+        //Final 3
+        } else if(resposta.equalsIgnoreCase(choise2cap3)){
                 System.out.println(cap5);
         }
     }
