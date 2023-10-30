465369b25073d74e20be6f5f943f7a9c0b82c250
Victor Rodrigues <victor.rodrigueslima@ufrpe.br>
2023-10-30 16:25:54 -0300

    Alteração em método capFinal

--- livro_interativo/Livro_interativo.java
+++ livro_interativo/Livro_interativo.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 
 class Livro_interativo {
 
-        public static void mostrarTexto(String capitulo, String opcao1, String opcao2){
+        public static void mostrarTexto(String capitulo, String opcao1, String opcao2) {
                 System.out.println("\n" + capitulo + "\n");
                 System.out.println(opcao1 + " | " + opcao2 + "\n");
+        }
+        public static void capFinal(String textoFinal){
+                System.out.println("\n" + textoFinal + "\n");
         }
 
         public static void main(String[] args) throws Exception {
@@ -40,52 +43,54 @@ class Livro_interativo {
                 String choisecap1 = "Final 1"; // Para o final 1
                 String choise2cap1 = "Cap3"; // Para o cap3
                 // Textos para o capítulo 2
-                String cap2 = "\nFinal 1 teste";
+                String cap2 = "Final 1 teste";
                 // Textos para o capítulo 3
                 String cap3 = "Começo do capítulo 3";
                 String choisecap3 = "Final 2"; // Para o final 2
                 String choise2cap3 = "Final 3"; // Para o final 3
                 // Textos para o capítulo 4
-                String cap4 = "\nFinal 2";
+                String cap4 = "Final 2";
                 // Textos para o capítulo 5
-                String cap5 = "\nFinal 3";
+                String cap5 = "Final 3";
 
                 // Começo do Cap 1
                 mostrarTexto(cap1, choisecap1, choise2cap1);
                 boolean escolhaValida = false;
-                
-                while(!escolhaValida){
-                        
+
+                while (!escolhaValida) {
+
                         resposta = leitor.nextLine();
 
                         // Final 1
-                         if (resposta.equalsIgnoreCase(choisecap1)) {
-                                System.out.println(cap2);
+                        if (resposta.equalsIgnoreCase(choisecap1)) {
+                                capFinal(cap2);
                                 System.exit(0);
-                        }       
+                        }
                         // Capítulo 3
                         else if (resposta.equalsIgnoreCase(choise2cap1)) {
                                 mostrarTexto(cap3, choisecap3, choise2cap3);
                                 escolhaValida = true;
                                 resposta = leitor.nextLine();
 
-                        // Final 2
-                        }       if (resposta.equalsIgnoreCase(choisecap3)) {
-                                System.out.println(cap4);
+                        }
+                                // Final 2
+                                if (resposta.equalsIgnoreCase(choisecap3)) {
+                                capFinal(cap4);
                                 System.exit(0);
 
-                        // Final 3
-                        } else if (resposta.equalsIgnoreCase(choise2cap3)) {
-                                System.out.println(cap5);
+                              }       
+                                // Final 3
+                                else if (resposta.equalsIgnoreCase(choise2cap3)) {
+                                capFinal(cap5);
                                 System.exit(0);
                         }
-                // Tratamento de entrada inválida
-                else{
-                        escolhaValida = false;
-                        System.out.println("Resposta inválida, por favor escolher" +
-                        " uma resposta que o programa mostrou"); 
+                        // Tratamento de entrada inválida
+                        else {
+                                escolhaValida = false;
+                                System.out.println("Resposta inválida, por favor escolher" +
+                                                " uma resposta que o programa mostrou");
+                        }
                 }
-         } 
                 leitor.close();
         }
 }
