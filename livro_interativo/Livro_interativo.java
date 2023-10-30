f32907aefe6fa2e96bae0f1afd5b0bf02b2f99a7
Victor Rodrigues <victor.rodrigueslima@ufrpe.br>
2023-10-30 11:52:23 -0300

    Teste da lógica e estrutura

--- livro_interativo/Livro_interativo.java
+++ livro_interativo/Livro_interativo.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 class Livro_interativo {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá! Você está em um livro interativo criado pelo\n" +
-                "aluno Victor Rodrigues, eu gostaria de informar que existem 2 tipos de\n" +
-                "interação: * significa Ação escolhida ou - significa Fala escolhida.\n" +
+                "aluno Victor Rodrigues, eu gostaria de informar\n" +
+                "que existem 2 tipos de interação nesse livro: \n" +
+                "'*' significa AÇÃO PARA O PERSONAGEM \n" +
+                "'-' significa FALA PARA O PERSONAGEM.\n" +
                 "\nVamos começar?  -  Sim  - Nao\n");
         //Criação de um scanner com o nome Start
-        Scanner start = new Scanner(System.in);
-        String init = start.nextLine();
+        Scanner leitor = new Scanner(System.in);
+        String init = leitor.nextLine();
         //Começar ou não o programa
         if (init.equalsIgnoreCase("sim")) {
         }
@@ -18,31 +20,39 @@ class Livro_interativo {
             System.out.println("\nTudo bem, fica para uma próxima :(\n");
             System.exit(0);
         }
-        //Corpo com a história
+        //Capta todas as respostas
                 String resposta;
 
+        //Corpo com a história
                 String cap1 = "Começo do capítulo 1";
-                String choisecap1 = "Primeira escolha";
-                String chois2cap1 = "Segunda escolha";
+                String choisecap1 = "Vai para o final 1"; // Para o final 1
+                String choise2cap1 = "Vai para o cap3"; // Para o cap3
 
                 String cap2 = "Começo do capítulo 2 - Final 1";
 
                 String cap3 = "Começo do capítulo 3";
-                String choisecap3 = "Primeira escolha";
-                String chois2cap3 = "Segunda escolha";
+                String choisecap3 = "Vai para o final 2"; // Para o final 2
+                String choise2cap3 = "Vai para o final 3"; // Para o final 3
 
                 String cap4 = "Começo do capítulo 4 - Final 2";
-                String choisecap4 = "Primeira escolha";
-                String chois2cap4 = "Segunda escolha";
 
                 String cap5 = "Começo do capítulo 4 - Final 3";
-                String choisecap5 = "Primeira escolha";
-                String chois2cap5 = "Segunda escolha";
-                
-                System.out.println("");
-                System.out.println("");
-                System.out.println("");
 
-
+        // Começo do Cap 1        
+        System.out.println("\n" + cap1 + "\n");
+        System.out.println(choisecap1 + " | " + choise2cap1 + "\n");
+        // Registrando escolha
+        resposta = leitor.nextLine();
+        if(resposta == choisecap1){
+          System.out.println(cap2);
+        } else if(resposta == choise2cap1){
+                System.out.println("\n" + cap3 + "\n");
+                System.out.println(choisecap3 + " | " + choise2cap3 + "\n");
+                resposta = leitor.nextLine();       
+        } if(resposta == choisecap3){
+                System.out.println(cap4);
+        } else if(resposta == choise2cap3){
+                System.out.println(cap5);
+        }
     }
 }
